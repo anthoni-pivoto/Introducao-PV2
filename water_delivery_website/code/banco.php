4cb8fe8b04d58651a03952262effeb1c35ac5297
anthoni-pivoto <anthonipivoto@gmail.com>
2023-05-31 18:49:26 -0300

    Tentando conexao com banco de dados de outra forma

--- water_delivery_website/code/banco.php
+++ water_delivery_website/code/banco.php
@@ -1,43 +1,14 @@
 <?php
 
-$bd_host = "192.168.20.18";
-$sgbd = "pgsql";
+$bd_host = "200.19.1.18";
+$bd_port = 5432;
 $base_de_dados = "anthonimigliavasca";
 $bd_usuario = "anthonimigliavasca";
 $bd_senha = "123456";
 
-switch ($sgbd) {
-    case "mysql":
-		try {
-			$dsn_mysql = "mysql:host=".$bd_host.";dbname=".$base_de_dados;
-			$conn = new PDO($dsn_mysql, $bd_usuario, $bd_senha);
-			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch(PDOException $e) {
-			die('ERROR: ' . $e->getMessage());
-		}
-        break;
-    case "pgsql":
-		try {
-			$dsn_pgsql = "pgsql:host=$bd_host;port=5432;dbname=$base_de_dados;";
-			// make a database connection
-			$conn = new PDO(
-				$dsn_pgsql,
-				$bd_usuario,
-				$bd_senha,
-				[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-			);
-		} catch (PDOException $e) {
-			die($e->getMessage());
-		}
-        break;
-    case "sqlite":
-
-		$conn = new PDO('sqlite:./sql/catalogo_de_games.sqlite3');
-		// Set errormode to exceptions
-		$conn->setAttribute(PDO::ATTR_ERRMODE, 
-									PDO::ERRMODE_EXCEPTION);
-							
-        break;
+$conexao = pg_connect("host=$bd_host port=$bd_port dbname=$base_de_dados " .
+								"user=$bd_usuario password=$bd_senha");
+if(!$conexao) {
+	die("Não foi possível se conectar ao banco de dados.");
 }
-
 ?>
