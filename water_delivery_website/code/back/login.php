e264843ad1693d52d54dfe62c023ae5ab0718981
anthoni-pivoto <anthonipivoto@gmail.com>
2023-06-27 20:18:24 -0300

    session and showing user info

--- water_delivery_website/code/back/login.php
+++ water_delivery_website/code/back/login.php
@@ -13,19 +13,19 @@ if(isset($_POST['myemail']) || isset($_POST['password'])){
         $email = pg_escape_string($_POST['myemail']);
         $senha = pg_escape_string($_POST['password']);
 
-        $sql = "select sh_senha, em_email from tb_pessoa where em_email ilike '$email' and sh_senha = '$senha'";
+        $sql = "select * from tb_pessoa where em_email ilike '$email' and sh_senha = '$senha'";
         $insertion = pg_query($conn,$sql) or die("Falha na conexao do codigo SQL" . pg_last_error());
         $qtd = pg_num_rows($insertion);
         if($qtd == 1){
-             $usuario = pg_fetch_assoc($insertion);
-             if(!isset($_SESSION)){
-                session_start();
-             }
-             $_SESSION['id'] = $usuario['cd_pessoa'];
-             $_SESSION['email'] = $usuario['em_email'];
-             $_SESSION['nome'] = $usuario['nm_pessoa'];
+            $usuario = pg_fetch_assoc($insertion);
+            if(!isset($_SESSION)){
+               session_start();
+            }
+            $_SESSION['id'] = $usuario['cd_pessoa'];
+            $_SESSION['email'] = $usuario['em_email'];
+            $_SESSION['nome'] = $usuario['nm_pessoa'];
 
-             header("Location: ../front/logged.php");
+            header("Location: ../front/logged.php");
             }
         else{
             echo "Email ou senha incorretos";
